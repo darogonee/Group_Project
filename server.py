47df1cef78fc7c7dfc6eccfe432bfa6e69929cb6
Dragonee <Dragonee2007@gmail.com>
2023-08-29 19:55:44 +1000

    replace

--- server.py
+++ server.py
@@ -10,13 +10,9 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("<a href='https://www.andrews.edu/~theron/red%20page.html'>Red Button</a>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
-
+        with open("index.html", "rb") as file:
+            self.wfile.write(file.read().replace(b"hi", b"by"))
+        
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
