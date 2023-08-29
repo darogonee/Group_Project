c633d1c67a3e5ebf5271ec1b49f52a0c796a7eb9
Dragonee <Dragonee2007@gmail.com>
2023-08-29 20:32:58 +1000

    added css and added more html and buttoin

--- server.py
+++ server.py
@@ -1,24 +1,36 @@
 # Python 3 server example
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
+import time, os
+
 
 hostName = "localhost"
 serverPort = 8080
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        with open("index.html", "rb") as file:
-            self.wfile.write(file.read().replace(b"hi", b"by"))
-        
+        match self.path:
+            case  "/":
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                with open("index.html", "rb") as file:
+                    self.wfile.write(file.read().replace(b"hi", b"by"))
+            case "/main.css":
+                self.send_response(200)
+                self.send_header("Content-type", "text/css")
+                self.end_headers()
+                with open("main.css", "rb") as file:
+                    self.wfile.write(file.read())
+            case "/stop":
+                webServer.server_close()
+                webServer.shutdown()
+                os.abort()
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
 
     try:
-        webServer.serve_forever()
+        webServer.serve_forever(1)
     except KeyboardInterrupt:
         pass
 
