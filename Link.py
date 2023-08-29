733f914a94f34c33f01bf5524943a60bbb5348d2
dragonee <dragonee2007@gmail.com>
2023-08-29 14:21:40 +1000

    error line

--- Link.py
+++ Link.py
@@ -2,3 +2,6 @@ import webbrowser
 
 webbrowser.open('https://trello.com/invite/b/dNxU4Y4T/ATTI19ddf455ab29e97f0fad4b194b4a8e87AA058D03/group-project')
 # might be useful not shore
+
+
+# error 429 its fine 
