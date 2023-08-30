f06f731c50d925ac29e6e47f6fb6698ad11d0ea2
Dragonee <Dragonee2007@gmail.com>
2023-08-30 19:57:56 +1000

    Api nearly working

--- Api.py
+++ Api.py
@@ -1,61 +1,43 @@
-import requests
-import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+import requests, json, urllib3
 
-auth_url = "https://www.strava.com/oauth/token"
-activites_url = "https://www.strava.com/api/v3/athlete/activities"
+client_id = "112868"
+client_secret = "6357af21299074aba1ea779a5edac54131293873"
+code = "edbe9c425bc0c0c19a5580502ff01553f429ad1d"
 
-payload = {
-    'client_id': "112868",
-    'client_secret': '6357af21299074aba1ea779a5edac54131293873',
-    'refresh_token': 'b8b94c28acd58d39f433bf3660c535d7f632f94d',
-    'grant_type': "refresh_token",
-    'f': 'json'
-}
-
-print("Requesting Token...\n")
-res = requests.post(auth_url, data=payload, verify=False)
-print(res.json())
-access_token = res.json()['access_token']
+# res = requests.post(f"https://www.strava.com/oauth/token?client_id={client_id}&client_secret={client_secret}&code={code}&grant_type=authorization_code")
+# with open("respones.json", "w") as file:
+#     json.dump(res.json(), file)
 
 
-print("Access Token = {}\n".format(access_token))
-header = {'Authorization': 'Bearer ' + access_token}
+refresh_toekn = "003ec2f9a8d3bdc38a569989fda4a6ce595c4988"
 
-# The first loop, request_page_number will be set to one, so it requests the first page. Increment this number after
-# each request, so the next time we request the second page, then third, and so on...
-request_page_num = 1
-print(request_page_num)
-all_activities = []
-print(all_activities)
+res = requests.post(f"https://www.strava.com/oauth/token?client_id={client_id}&client_secret={client_secret}&refresh_token={refresh_toekn}&grant_type=refresh_token")
+with open("respones.json", "w") as file:
+    json.dump(res.json(), file)
 
 
-# FIXME this part seems unnessasery
-while True:
-    param = {'per_page': 200, 'page': request_page_num}
-    print(param)
-    # initial request, where we request the first page of activities
-    my_dataset = requests.get(activites_url, headers=header, params=param).json()
-    print(my_dataset)
-    # check the response to make sure it is not empty. If it is empty, that means there is no more data left. So if you have
-    # 1000 activities, on the 6th request, where we request page 6, there would be no more data left, so we will break out of the loop
-    if len(my_dataset) == 0:
-        print("breaking out of while loop because the response is zero, which means there must be no more activities")
-        break
 
-    # if the all_activities list is already populated, that means we want to add additional data to it via extend.
-    if all_activities:
-        print("all_activities is populated")
-        all_activities.extend(my_dataset)
+# urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-    # if the all_activities is empty, this is the first time adding data so we just set it equal to my_dataset
-    else:
-        print("all_activities is NOT populated")
-        all_activities = my_dataset
+# auth_url = "https://www.strava.com/oauth/token"
+# activites_url = "https://www.strava.com/api/v3/athlete/activities"
 
-    request_page_num += 1
+# payload = {
+#     'client_id': client_id,
+#     'client_secret': client_secret,
+#     'refresh_token': refresh_toekn,
+#     'grant_type': "refresh_token",
+#     'f': 'json'
+# }
 
-print(len(all_activities))
-for count, activity in enumerate(all_activities):
-    print(activity["name"])
-    print(count)
+# print("Requesting Token...\n")
+# res = requests.post(auth_url, data=payload, verify=False)
+# access_token = res.json()['access_token']
+# print("Access Token = {}\n".format(access_token))
+
+# header = {'Authorization': 'Bearer ' + access_token}
+# param = {'per_page': 200, 'page': 1}
+# my_dataset = requests.get(activites_url, headers=header, params=param).json()
+
+# with open("activite.json", "w") as file:
+#     json.dump(my_dataset, file)
--- Test.py
+++
@@ -1,2 +0,0 @@
-print("Oliver you are gay")
-
