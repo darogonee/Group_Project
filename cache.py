f84014bd9140b8d9693fb879080d928b13056268
Dragonee <Dragonee2007@gmail.com>
2023-09-22 21:31:58 +1000

    expire data stuff

---
+++ cache.py
@@ -0,0 +1,53 @@
+from datetime import datetime, timedelta
+import time
+
+def cache(max_age=60):
+    """
+    A decorator to cache the result of a function call for a given time.
+    :param max_age: the maximum time to cache the result in seconds
+    """
+    # create a decorator
+    def decorator(function):
+        # create a wrapper to run the function or return cached result
+        class AgeCacheWrapper:
+            cache = {}
+            @staticmethod
+            def clear(*args, **kwargs):
+                # clear the cache for the given arguments
+                AgeCacheWrapper.cache.pop((args, tuple(kwargs.items())))
+            @staticmethod
+            def __call__(*args, **kwargs):
+                # find the cache key
+                cache_key = (args, tuple(kwargs.items()))
+                # check if the cache key is in the cache
+                if cache_key in AgeCacheWrapper.cache:
+                    result, timestamp = AgeCacheWrapper.cache[cache_key]
+                    # if not expired, return the cached result
+                    if datetime.now() < timestamp + timedelta(seconds=max_age):
+                        return result
+                # run the function and cache the result
+                result = function(*args, **kwargs)
+                # store the result with the timestamp
+                AgeCacheWrapper.cache[cache_key] = (result, datetime.now())
+                # return the result after caching
+                return result
+        # return the wrapper
+        return AgeCacheWrapper()
+    # return the decorator created
+    return decorator
+
+if __name__ == "__main__":
+    @cache(max_age=10)
+    def sleep(awake):
+        time.sleep(1)
+        return awake
+    
+    print(sleep(2))
+    print(sleep(1))
+    print(sleep(3))
+    print(sleep(2))
+    print(sleep(1))
+    sleep.clear(1)
+    print(sleep(1))
+    print(sleep(3))
+    print(sleep(2))
