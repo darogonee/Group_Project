06b3b9d91dd3ecc013a9c41dd17bb1c4938e4b24
charliegregg <charliejamesgregg@gmail.com>
2023-09-22 22:18:07 +1000

    clean up code and remove timeout race condition bugs kinda

--- main.js
+++ main.js
@@ -4,57 +4,52 @@ function logout() {
 }
 
 function addToTable() {
-    const addRow = document.getElementById("add-to-table");
     const tableBody = document.querySelector("#tb tbody");
+    const newRow = document.createElement("tr");
+    const exerciseCell = document.createElement("td");
+    const repstimeCell = document.createElement("td");
+    const setsCell = document.createElement("td");
+    const restCell = document.createElement("td");
 
+    const exerciseInput = document.createElement("input");
+    const repstimeInput = document.createElement("input");
+    const setsInput = document.createElement("input");
+    const restInput = document.createElement("input");
+    
+    // make names not the same between rows
+    // so that they don't overwrite each other
+    exerciseInput.type = "text"
+    exerciseInput.name = "exercise_input"
 
-    addRow.addEventListener("click", function () {
-        const newRow = document.createElement("tr");
-        const exerciseCell = document.createElement("td");
-        const repstimeCell = document.createElement("td");
-        const setsCell = document.createElement("td");
-        const restCell = document.createElement("td");
+    repstimeInput.type = "number"
+    repstimeInput.min = "0"
+    repstimeInput.name = "repstime_input"
 
-        const exerciseInput = document.createElement("input");
-        const repstimeInput = document.createElement("input");
-        const setsInput = document.createElement("input");
-        const restInput = document.createElement("input");
-        
-        // make names not the same betwwen rows
-        // so that they dohnt over right each other
-        exerciseInput.type = "text"
-        exerciseInput.name = "exercise_input"
+    setsInput.type = "number"
+    setsInput.name = "sets_input"
+    setsInput.min = "0"
 
-        repstimeInput.type = "number"
-        repstimeInput.min = "0"
-        repstimeInput.name = "repstime_input"
+    restInput.type = "number"
+    restInput.name = "rest_input"
+    restInput.min = "0"
 
-        setsInput.type = "number"
-        setsInput.name = "sets_input"
-        setsInput.min = "0"
+    const deleteRow = document.createElement("button")
+    deleteRow.textContent = "Delete"
+    deleteRow.addEventListener("click", function(){
+        tableBody.removeChild(newRow);
+    });
 
-        restInput.type = "number"
-        restInput.name = "rest_input"
-        restInput.min = "0"
+    exerciseCell.appendChild(exerciseInput)
+    repstimeCell.appendChild(repstimeInput)
+    setsCell.appendChild(setsInput)
+    restCell.appendChild(restInput)
+    exerciseCell.appendChild(deleteRow)
 
-        const deleteRow = document.createElement("button")
-        deleteRow.textContent = "Delete"
-        deleteRow.addEventListener("click", function(){
-            tableBody.removeChild(newRow);
-        });
+    
+    newRow.appendChild(exerciseCell)
+    newRow.appendChild(repstimeCell)
+    newRow.appendChild(setsCell)
+    newRow.appendChild(restCell)
 
-        exerciseCell.appendChild(exerciseInput)
-        repstimeCell.appendChild(repstimeInput)
-        setsCell.appendChild(setsInput)
-        restCell.appendChild(restInput)
-        exerciseCell.appendChild(deleteRow)
-
-        
-        newRow.appendChild(exerciseCell)
-        newRow.appendChild(repstimeCell)
-        newRow.appendChild(setsCell)
-        newRow.appendChild(restCell)
-
-        tableBody.appendChild(newRow);
-    })
+    tableBody.appendChild(newRow);
 };
