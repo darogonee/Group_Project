794a8ce2fe58d75908f645ef19a1435de41c7f88
Frogjam912 <143458323+Frogjam912@users.noreply.github.com>
2023-09-28 09:50:45 +1000

    fix git

---
+++ Nutrition_Calculator.py
@@ -0,0 +1,10 @@
+import requests
+def nutrition_calculator(food):
+    api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(food)
+    response = requests.get(api_url, headers={'X-Api-Key': 'f+eHE4GyqdBw4+qAdCmEag==9wegAJ3ahmSJIHw4'})
+    if response.status_code == requests.codes.ok:
+        print(response.text)
+    else:
+        print("Error:", response.status_code, response.text)
+    return response
+
