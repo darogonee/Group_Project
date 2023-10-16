46d5717fba06780571fcee7b4473d58ea12671ad
ATex0910 <trong-son_tran25@student.friends.tas.edu.au>
2023-10-16 14:48:01 +1100

    Nutri

--- python/requirements.py
+++ python/requirements.py
@@ -1,4 +1,32 @@
 
+from datetime import date
+
+def calculateAge(born):
+    today = date.today()
+    
+    try:
+
+        birthday = born.replace(year = today.year)
+ 
+    except ValueError:
+        birthday = born.replace(year = today.year,
+                  month = born.month + 1, day = 1)
+ 
+    if birthday > today:
+        return today.year - born.year - 1
+    else:
+        return today.year - born.year
+         
+# Driver code
+listtt = []
+listt = input().split("-")
+for val in listt:
+    listtt.append(int(val))
+print(calculateAge(input(tuple(listtt))))
+
+
+print(date(1997, 2, 3))
+
 def calculate_eer(age, weight, height, sex, pal):
     bmi = weight/(height^2)
     if bmi < 25:
