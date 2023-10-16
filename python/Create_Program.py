0cdca5f5e3d8c6147c7a66ebf6b3c3998e639dff
oliver-magill <oliver_magill25@student.friends.tas.edu.au>
2023-10-16 19:02:44 +1100

    program

--- python/Create_Program.py
+++ python/Create_Program.py
@@ -1,8 +1,24 @@
-import random
+# import random
 import json
+import random
 
 def create_program(data):
-    program = {"monday":None, "tuesday":None, "wednesday":None, "thursday":None, "friday":None, "saturday":None}
+    program = {"monday":None, "tuesday":None, "wednesday":None, "thursday":None, "friday":None, "saturday":None, "sunday":None}
+    cardio = False
+    weights = False
+    cardio_weights_split = True
+    fitness_goals = []
+    equipment = [""]
+
+    weights_training_days = []
+    cardio_training_days = []
+    training_days = []
+    weights_days_count = 0
+    cardio_days_count = 0
+    training_days_count = 0 
+
+    level = data["level"]
+
     weight_programs = {
     1: [
         [
@@ -185,36 +201,12 @@ def create_program(data):
 
     ]
     }
-    #3:"ppl", 4:"upper-lower-twice", 5:"ulppl", 6:"ppl-twice"}
-    fitness_goals = []
-    equipment = [""]
-    training_days = []
-    weights = False
-    cardio = False
-    level = data["level"]
+
+
 
     for key,value in data["fitness-goals"].items():
         if value:
             fitness_goals.append(key)
-
-    for key,value in data["equipment"].items():
-        if value:
-            equipment.append(key)
-
-    for key,value in data["training_days"].items():
-        if value:
-            training_days.append(key)
-
-    print(training_days)
-
-    # rest day if all days ticked
-    if len(training_days) > 6:
-        rest_day = random.choice(training_days)
-        training_days.remove(rest_day)
-
-    #available days True and unavailable days False
-    for day in program.keys():
-        program[day] = day in training_days
 
     if "endurance" in fitness_goals or "strength" in fitness_goals or "hypertrophy" in fitness_goals:
         weights = True
@@ -222,18 +214,66 @@ def create_program(data):
     if "cardio" in fitness_goals:
         cardio = True
 
+
+    for goal in fitness_goals:
+        if goal == "cardio":
+            cardio = True
+        else:
+            weights = True
+
+    if cardio and weights:
+        cardio_weights_split = True
+
+    for key,value in data["equipment"].items():
+        if value:
+            equipment.append(key)
+
+    # rest day if all days ticked
+    if len(training_days) > 6:
+        rest_day = random.choice(training_days)
+        training_days.remove(rest_day)
+
+    training_days_count = len(training_days)
+
+    #available days True and unavailable days False
+    for day in program.keys():
+        program[day] = day in training_days
+
+    for day, workout in program.items():
+        if workout:
+            # If we haven't assigned an equal number of "True" days to both cardio and weights yet
+            if cardio_days_count < training_days_count // 2:
+                program[day] = "cardio"
+                cardio_count += 1
+            else:
+                program[day] = "weights"
+                weights_days_count += 1
+
+
+
+    # get number of training day
+    # if cardio_weights_split == True 
+    # then weights_training_days_count = upper(training_days_count/2)
+    # and cardio_training_days_count = lower(training_days_count/2)
+
+    # assign days to cardio or weights or None
+
+   
+
+
+    
+
     #5-8 exercises each workout
 
-    training_days_count = 2
-    muscle_groups = weight_programs[training_days_count] # list or list of lists
+    muscle_groups = weight_programs[weights_days_count] # list or list of lists
     exercises = []
     for split in muscle_groups:
         exercises.append(get_exercises(valid_exercises(level, equipment), "muscle_group", split))
 
-    for i in range(training_days_count):
+    for i in range(weights_days_count):
         program[training_days[i]] = exercises[i]
 
-    print(program)
+    return program
 
     
 
@@ -242,7 +282,7 @@ def valid_exercises(user_level, user_equipment):
     with open("data/exercises.json", "r") as file:
         exercise_data = json.load(file)
 
-    get_levels = {"Beginner":"Beginner", "Intermediate":["Beginner", "Intermediate"], "Advanced":["Beginner", "Intermediate", "Advanced"]}
+    get_levels = {"beginner":"beginner", "intermediate":["beginner", "intermediate"], "advanced":["beginner", "intermediate", "advanced"]}
     level_filtered_data = []
 
     for level in get_levels[user_level]:
@@ -278,57 +318,3 @@ def get_exercises(valid_exercises, filter_type, values):
             exercises.append(None)
     return exercises
 
-# print(get_exercises(valid_exercises("Advanced", [""]),"muscle_group", ["Chest", "Quadriceps"]))
-
-create_program({
-    "fitness-goals": {
-        "cardio": True,
-        "strength": True,
-        "hypertrophy": False,
-        "endurance": False
-    },
-    "fav-cardio": "Running",
-    "level": "Beginner",
-    "weight-goal": "gain",
-    "weight-units": "kg",
-    "weight": "78",
-    "height-units": "cm",
-    "height": "186",
-    "dob": "2007-12-13",
-    "sex": "male",
-    "equipment": {
-        "bench": True,
-        "medicine-ball": False,
-        "cable-machine": True,
-        "torso-rotation-machine": False,
-        "ab-roller": False,
-        "dumbbell": True,
-        "barbell": True,
-        "assisted-pullup-machine": False,
-        "lat-pulldown-machine": True,
-        "pullup-bar": True,
-        "v-bar": False,
-        "machine-row": True,
-        "ez-bar": True,
-        "preacher-curl-machine": False,
-        "rope": True,
-        "leg-press-machine": False,
-        "smith-machine": False,
-        "calf-raise-machine": False,
-        "chest-press-machine": True,
-        "bench-press-machine": True,
-        "plates": True,
-        "dip-assist-machine": False,
-        "dip-machine": False
-    },
-    "training_days": {
-        "monday": True,
-        "tuesday": True,
-        "wednesday": False,
-        "thursday": False,
-        "friday": False,
-        "saturday": False,
-        "sunday": True
-    },
-    "rhr": "53"
-})
