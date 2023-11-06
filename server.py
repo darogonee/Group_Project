6f54ee608701a2d3531ae4505765b5ec0375333e
oliver-magill <oliver_magill25@student.friends.tas.edu.au>
2023-11-06 14:44:43 +1100

    yd

--- server.py
+++ server.py
@@ -31,30 +31,30 @@ serverPort = 8080
 uuid2user = {}
 
 class FittnessServer(BaseHTTPRequestHandler):
-    # def _set_response(self):
-    #     self.send_response(200)
-    #     self.send_header('Content-type', 'text/html')
-    #     self.end_headers()
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
 
-    # def do_POST(self):
-    #     content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-    #     post_data = self.rfile.read(content_length) # <--- Gets the data itself
-    #     content = json.loads(post_data.decode('utf-8'))
-    #     match content["title"]:
-    #         case "edit-json":
-    #             data = json.load("data/"+content["body"][0])
-    #             del data["food_log"][content["body"][1]]
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        content = json.loads(post_data.decode('utf-8'))
+        match content["title"]:
+            case "edit-json":
+                data = json.load(open("data/"+content["body"][0]))
+                del data["food_log"][content["body"][1]]
 
-    #             response = "deletion-successful"
-    #         case _:
-    #             response = "err:invalid-title"
+                response = "deletion-successful"
+            case _:
+                response = "err:invalid-title"
 
 
-    #     self._set_response()
-    #     if type(response) != type(""):
-    #         response = json.dumps(response)
-    #     self.wfile.write(bytes('{"response":'+response+',"title":"'+content["title"]+'"}',"utf-8"))
-    #     print("request fulfilled")
+        self._set_response()
+        if type(response) != type(""):
+            response = json.dumps(response)
+        self.wfile.write(bytes('{"response":'+response+',"title":"'+content["title"]+'"}',"utf-8"))
+        print("request fulfilled")
 
     def redirect(self, link):
         self.send_response(200)
@@ -199,7 +199,7 @@ class FittnessServer(BaseHTTPRequestHandler):
                 with open("data/nutritionlog.json", "w") as file:
                     json.dump({}, file)
 
-                with open(f"user_data/{user}.json", "r") as user_data_file:
+                with open(f"nutrition_data/{user}.json", "r") as user_data_file:
                     user_data = json.load(user_data_file)
 
                 try:
@@ -208,7 +208,7 @@ class FittnessServer(BaseHTTPRequestHandler):
                 except KeyError:
                     pass
             
-                with open(f"user_data/{user}.json", "w") as write_user_data_file:
+                with open(f"nutrition_data/{user}.json", "w") as write_user_data_file:
                     json.dump(user_data, write_user_data_file)
 
 
@@ -235,7 +235,11 @@ class FittnessServer(BaseHTTPRequestHandler):
                 except:
                     data = {"date":date, "food_log":[]}
 
-                with open(f"user_data/{user}.json", "r") as user_data_file:
+                if not os.path.exists(f"nutrition_data/{user}.json"):
+                    with open(f"nutrition_data/{user}.json", "w") as file:
+                        json.dump({}, file)
+
+                with open(f"nutrition_data/{user}.json", "r") as user_data_file:
                     user_data = json.load(user_data_file)
 
                 if "nutrition_log" in user_data.keys():
@@ -258,7 +262,7 @@ class FittnessServer(BaseHTTPRequestHandler):
                             
                         user_data["nutrition_log"][date]["data_transferred"] = "True"
 
-                        with open(f"user_data/{user}.json", "w") as write_user_data:
+                        with open(f"nutrition_data/{user}.json", "w") as write_user_data:
                             json.dump(user_data, write_user_data)
 
                 try:
@@ -317,7 +321,7 @@ class FittnessServer(BaseHTTPRequestHandler):
                 with open("data/nutritionlog.json", "r") as nutrition_log:
                     logged_data = json.load(nutrition_log)
 
-                with open(f"user_data/{user}.json", "r") as user_data_file:
+                with open(f"nutrition_data/{user}.json", "r") as user_data_file:
                     user_data = json.load(user_data_file)
 
                 total_calories = str(round(sum(float(item["calories"]) for item in logged_data["food_log"]), 1))
@@ -331,7 +335,7 @@ class FittnessServer(BaseHTTPRequestHandler):
                 
                 user_data["nutrition_log"][date] = {"food":logged_data["food_log"], "totals":{"total_calories":total_calories, "total_carbs":total_carbs, "total_fat":total_fat, "total_protein":total_protein}}
 
-                with open(f"user_data/{user}.json", "w") as user_data_file:
+                with open(f"nutrition_data/{user}.json", "w") as user_data_file:
                     json.dump(user_data, user_data_file)
 
                 
